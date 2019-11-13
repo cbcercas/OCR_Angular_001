66c0d8199681c48a083e23d39f269b5143858c60
Christopher BRAVO CERCAS <christopher@cercas.fr>
2019-11-13 13:01:38 +0100

    Add Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import {PostListComponent} from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import {PostsService} from './services/posts.service';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     PostListItemComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
