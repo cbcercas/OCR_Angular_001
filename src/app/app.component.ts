8204fe99d027149b778888dc83d07b7ee1d1982a
Christopher BRAVO CERCAS <christopher@cercas.fr>
2019-11-13 20:27:55 +0100

    Add Firebase configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mon Blog';
+
+  // Your web app's Firebase configuration
+  constructor() {
+    var firebaseConfig = {
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
