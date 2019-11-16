ddc0c6a9e3ebcd5186212c8ae3d0273e680c0dca
Christopher BRAVO CERCAS <christopher@cercas.fr>
2019-11-16 15:18:01 +0100

    Firebase config for correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   // Your web app's Firebase configuration
   constructor() {
     var firebaseConfig = {
+      apiKey: "AIzaSyDrvu7JavgfCAC32RSfUvcuMx4_jLxcASc",
+      authDomain: "blog-a9b4c.firebaseapp.com",
+      databaseURL: "https://blog-a9b4c.firebaseio.com",
+      projectId: "blog-a9b4c",
+      storageBucket: "blog-a9b4c.appspot.com",
+      messagingSenderId: "542261829768",
+      appId: "1:542261829768:web:06808c2ddb995282b9bf3d",
+      measurementId: "G-NDBQ7WEXNS"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
